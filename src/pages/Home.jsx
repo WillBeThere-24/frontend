d1536a83adab18b7b290890a10f8b8aaed51d67b
Sebastian Anioke <aniokechukwudi7@gmail.com>
2024-04-12 23:47:21 +0100

    added react router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+function Home() {
+  return (
+   <main className='w-screen h-screen flex justify-center items-center bg-zinc-500'>
+   <p className='text-5xl'>Will Be There</p>
+ </main>
+  )
+}
+export default Home
