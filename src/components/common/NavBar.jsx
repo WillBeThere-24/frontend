c551e38956fb5c0c0ec8b5df6fd1b6ec0235fae7
Idighekere <idighekereudo@gmail.com>
2024-04-13 19:36:10 +0100

    made modification on the navbar, added a logo

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { NavLink } from "react-router-dom"
 import { Harmburger, CloseMenuBtn } from "./svg"
+import WBT from "../../assets/WBT.png"
+import WillBeThere from "../../assets/Will be there.png"
 
 const styles = {
 
   navBarContainer: `flex h-[10vh] sticky top-0 justify-between w-screen gpx-[100px] items-center px-8 py-12 md:px-24 md:py-10 font-normal text-sm font-montserrat`,
-  logo: ``,
+  logo: ` justify-center flex flex-col items-center`,
   navBarLinks: ` text-wybt-primary justify-between gap-5 md:gap-10 sm:flex hidden`,
   navBarLinksMobile: `flex sm:hidden flex-col  z-10 gap-3`,
   navBarBtn: `bg-wybt-primary text-wybt-white /h-[58.9px]  rounded-md py-2 px-11 gap-4 sm:block hidden`,
   navBarBtnMobile: `bg-wybt-primary text-wybt-white /h-[58.9px] rounded-md py-2 px-10 gap-4 text-center flex sm:hidden `,
   // sideline: `bg-[#45062e] h-full w-2 p-[1px] `,
-  sidelineLeft: `flex-r md:border-l-[1px] border-wybt-neutralBlack md:pl-20 block pl-10`,
-  sidelineRight: `fle md:border-r-[1px] border-wybt-neutralBlack md:pr-20 block py-2 pr-10`,
+  sidelineLeft: `flex-r md:border-l-[1px] border-wybt-neutral-black md:pl-20 block pl-10 max-h-full py-1`,
+  sidelineRight: `fle md:border-r-[1px] border-wybt-neutral-black md:pr-20 block pr-10 py-[.12em]`,
   mobileNav: `flex flex-col top-0 bg-wybt-white p-20 w-3/4 absolute right-0 h-screen sm:hidden gap-5   `,
-  navBarOverlay: `bg-wybt-neutralBlack h-screen absolute top-0 w-screen left-0 opacity-80 cursor-pointer `
+  navBarOverlay: `bg-wybt-neutral-black h-screen absolute top-0 w-screen left-0 opacity-80 cursor-pointer `
 }
 const NavBar = () => {
 
@@ -31,7 +33,7 @@ const NavBar = () => {
     <>
       <nav className={styles.navBarContainer}>
         <div className="">
-          <span className={styles.sidelineRight}>WYBT</span>
+          <span className={styles.sidelineRight}><div className={styles.logo}><img src={WBT} alt="WBT Logo" width='64px' /><img src={WillBeThere} alt="WBT TagLine" className='w-20' /></div></span>
         </div>
         <div className={styles.navBarLinks}>
           <NavLink to="">Event Details</NavLink>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
           primary: "#45062E",
           secondary: "#8E085D",
           accent: "#E5A4CB",
-          white: "#F00917",
+          white: "#F8F7F9",
           neutral: "#838383",
           error: "#F00917",
           "neutral-white": "#E7E6E9",
