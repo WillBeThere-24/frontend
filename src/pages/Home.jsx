0d13d15f84eb3f1227b263cbd4deeb5b9cc1b0fc
Idighekere <idighekereudo@gmail.com>
2024-04-13 15:36:04 +0100

    Created Navbar Component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import Footer from "../components/common/Footer";
+import NavBar from "../components/common/NavBar";
 
 function Home() {
   return (
     <main className='w-screen h-screen justify-between flex flex-col gap-16 md:gap-32 bg-wybt-white'>
+      <NavBar />
       <p className='text-5xl'>Will Be There</p>
       <Footer />
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ export default {
         wybt: {
           primary: "#45062E",
           white: "#FFFFFF",
+          secondary: "#8E085D",
+          accent: "#E5A4CB",
+          neutralWhite: "#E7E6E9",
+          error: "#F00917",
+          neutralBlack: "#1F1A1D",
+          whiteBg: "#F8F7F9",
+          neutral: "#838383",
+          lightGrey: "#C2C2C2"
         },
       },
     },
