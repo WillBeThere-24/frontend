1176616b5aff7d963bc01f8f1b2c5d9498aa59f0
Idighekere <idighekereudo@gmail.com>
2024-04-17 16:30:29 +0100

    Created a page for the sign up/log in page

---
+++ src/pages/AccountSetup.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import Tabs from '../components/Tabs'
+// import tabsData from '../components/tabsData'
+// import TabsContent from '../components/TabsContent'
+import NavBar from '../components/common/NavBar'
+import Footer from '../components/common/Footer'
+import aPen from '../assets/A-pen-beside-a-note.png'
+
+const AccountSetup = () => {
+    // const data = tabsData
+
+    return (
+        <>
+            <NavBar /><div className='px-8 py-12 md:px-24 block md:flex gap-8 bg-wybt-white font-montserrat'>
+                <Tabs /><div className='md:flex items-center justify-center w-[350px] hidden z-0 m-auto '>
+                    <img src={aPen} alt="A pen beside a notebook" className='w-[350px] self-center' /></div>
+            </div>
+            <Footer /></>
+
+    )
+}
+
+export default AccountSetup
