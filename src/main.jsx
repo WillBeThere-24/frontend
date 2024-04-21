d1edf08227f56b9833de4050013728487c9a9700
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-21 20:54:38 +0100

    react hot toast

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ToastContainer, toast } from "react-toastify";
+import { Toaster } from "react-hot-toast";
 import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <ToastContainer />
+    <Toaster
+      position='bottom-right'
+      reverseOrder={false}
+      gutter={8}
+      containerClassName=''
+      containerStyle={{}}
+      toastOptions={{
+        duration: 3000,
+      }}
+    />
   </React.StrictMode>
 );
