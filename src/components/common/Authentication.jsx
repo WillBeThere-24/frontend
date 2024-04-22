2cc60facf482472183eaa58ffbdf866acbadec83
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-22 10:19:12 +0100

    authentication

--- src/components/common/Authentication.jsx
+++ src/components/common/Authentication.jsx
@@ -1,21 +1,34 @@
-import { toast } from "react-hot-toast";
+import toast from "react-hot-toast";
 import useStore from "../../utils/store/useStore";
-import { Navigate } from "react-router-dom";
-import { useEffect } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 
-const Authentication = ({ children }) => {
+const Authentication = () => {
   const user = useStore((state) => state.user);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    if (!user) {
-      toast.error("Please login to access this page");
+    setTimeout(() => setLoading(false), 2000);
+
+    if (!user && !loading) {
+      toast.error("User is not logged in \n Please login to access this page");
     }
-  }, [user]);
+
+    return () => clearTimeout(setTimeout(() => setLoading(false), 2000));
+  }, [user, loading]);
+
+  if (loading) {
+    return (
+      <div className='w-full h-screen flex justify-center items-center text-2xl font-bold text-wybt-light-gray'>
+        Checking if user is logged in...
+      </div>
+    );
+  }
 
   if (!user) {
     return <Navigate to='/register' />;
   }
 
-  return <>{children}</>;
+  return <Outlet />;
 };
 export default Authentication;
