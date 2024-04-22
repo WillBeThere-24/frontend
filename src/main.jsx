24cc2069483f1746caa7716fbdd2811707209d56
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-22 08:06:40 +0100

    little fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
-import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
