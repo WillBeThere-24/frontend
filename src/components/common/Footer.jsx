0c4fca77be17f17de4293db27ec2501a753ed66a
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-22 10:14:34 +0100

    svg fix

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,25 +1,18 @@
 import Button from "./Button";
-import {
-  CallIcon,
-  FacebookIcon,
-  InstagramIcon,
-  LocationIcon,
-  MsgIcon,
-  TwitterIcon,
-} from "./svg";
 import { Link } from "react-router-dom";
+import toast from "react-hot-toast";
 
 const footerIconsAndTitles = [
   {
-    icon: <CallIcon className='md:size-7 size-6' />,
+    icon: "/icons/footer/call.svg",
     text: "000-000-000",
   },
   {
-    icon: <MsgIcon className='md:size-7 size-6' />,
+    icon: "/icons/footer/msg.svg",
     text: "dummy@gmail.com",
   },
   {
-    icon: <LocationIcon className='md:size-7 size-6' />,
+    icon: "/icons/footer/location.svg",
     text: "Lagos, Nigeria",
   },
 ];
@@ -28,13 +21,16 @@ const Footer = () => {
     <footer className='flex flex-col gap-8 md:gap-4 w-full bg-wybt-primary text-wybt-white px-8 py-16 md:px-24 md:py-36 font-normal text-sm font-montserrat overflow-hidden'>
       <div className='flex flex-col md:flex-row gap-8 lg:gap-16 justify-between'>
         <div className='flex flex-col gap-6 flex-1'>
-          <Button className='text-wybt-primary bg-wybt-white'>
+          <Button
+            className='text-wybt-primary bg-wybt-white'
+            onClick={() => toast("yay")}
+          >
             Contact Us
           </Button>
           {footerIconsAndTitles.map((item, id) => {
             return (
               <div className='flex gap-4' key={id}>
-                {item.icon}
+                <img src={item.icon} alt={item.icon} />
                 <p>{item.text}</p>
               </div>
             );
@@ -52,14 +48,14 @@ const Footer = () => {
           <div className='flex flex-col gap-4 text-wybt-white justify-center'>
             <Link to='/'>Will you be there</Link>
             <div className='flex gap-2 self-end'>
-              <a href='/' target='_blank'>
-                <InstagramIcon className=' size-6' />
+              <a href='https://www.instagram.com' target='_blank'>
+                <img src='/icons/footer/instagram.svg' alt='instagram' />
               </a>
-              <a href='/' target='_blank'>
-                <FacebookIcon className='size-6' />
+              <a href='https://www.facebook.com' target='_blank'>
+                <img src='/icons/footer/facebook.svg' alt='facebook' />
               </a>
-              <a href='/' target='_blank'>
-                <TwitterIcon className='size-6' />
+              <a href='https://www.twitter.com' target='_blank'>
+                <img src='/icons/footer/twitter.svg' alt='twitter' />
               </a>
             </div>
           </div>
