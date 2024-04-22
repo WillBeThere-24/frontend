c0125feedcf721322168ef8e90b9d17f3f7a1a9f
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-22 10:15:28 +0100

    paths and sharedlayout fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import AccountSetup from "./pages/AccountSetup";
-import Dashboard from "./pages/Dashboard";
-import Profile from "./pages/Profile";
-import FormBuilder from "./pages/FormBuilder";
-import SidebarContainer from "./components/dashboard/SidebarContainer";
+import {
+  HomeSharedLayout,
+  Home,
+  AccountSetup,
+  FormBuilder,
+  Profile,
+  DashboardSharedLayout,
+} from "./pages";
+import { SidebarContainer } from "./components/dashboard";
+import { Authentication } from "./components/common";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/register",
-    element: <AccountSetup />,
+    element: <HomeSharedLayout />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/register", element: <AccountSetup /> },
+    ],
   },
   {
     path: "/dashboard",
-    element: <Dashboard />,
-  },
-  {
-    path: "/dashboard/profile",
-    element: <Profile />,
-  },
-  {
-    path: "/dashboard/form-builder",
-    element: <FormBuilder />,
+    element: <Authentication />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <DashboardSharedLayout />,
+        children: [
+          {
+            path: "/dashboard/profile",
+            element: <Profile />,
+          },
+          {
+            path: "/dashboard/form-builder",
+            element: <FormBuilder />,
+          },
+        ],
+      },
+    ],
   },
 ]);
 
