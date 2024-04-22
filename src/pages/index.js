8ebb32ed59f964f428dac26b69b74d231f069c65
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-22 08:06:48 +0100

    import fixing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 export { default as Home } from "./Home";
 export { default as AccountSetup } from "./AccountSetup";
 export { default as HomeSharedLayout } from "./HomeSharedLayout";
+export { default as Dashboard } from "./Dashboard";
+export { default as FormBuilder } from "./FormBuilder";
+export { default as Profile } from "./Profile";
