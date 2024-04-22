42ef28911fe936779b0f21df224af8eb41e3ec12
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-22 10:21:11 +0100

    removed import react

--- src/components/dashboard/common/Subpage.jsx
+++ src/components/dashboard/common/Subpage.jsx
@@ -1,19 +1,15 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 const Subpage = ({ title, icon, link, showText }) => {
+  // let { path, url } = useRouteMatch()
+  return (
+    <li className='flex gap-3 text-wybt-primary'>
+      <NavLink to={link} className='font-[500] flex gap-3'>
+        <img src={icon} alt='' />
+        {showText && <p className='hiden md:bloc'>{title}</p>}
+      </NavLink>
+    </li>
+  );
+};
 
-    // let { path, url } = useRouteMatch()
-    return (
-        <li className='flex gap-3 text-wybt-primary'>
-            <NavLink to={link} className='font-[500] flex gap-3'>
-                <img src={icon} alt="" />{showText &&
-                    <p className='hiden md:bloc'>{title}
-                    </p>
-                }
-            </NavLink>
-        </li>
-    )
-}
-
-export default Subpage
+export default Subpage;
