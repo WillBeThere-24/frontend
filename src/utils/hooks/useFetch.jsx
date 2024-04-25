8f181eec52d509c5853d7ffcae565a35806ea03f
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-25 06:08:52 +0100

    edited useFetch custom hook

--- src/utils/hooks/useFetch.jsx
+++ src/utils/hooks/useFetch.jsx
@@ -1,23 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
-const useFetch = () => {
+const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const fetchData = async (url) => {
-    setLoading(true);
-    try {
-      const { data } = await axios.get(url);
-      setData(data);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-  return { fetchData, data, loading, error };
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const { data } = await axios.get(url);
+        setData(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+  return { data, loading, error };
 };
 
 export default useFetch;
