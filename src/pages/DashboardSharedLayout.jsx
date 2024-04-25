c3e7979a3123b3b6d0e4fa1bec47204cb5d7bccd
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-25 06:08:37 +0100

    few changes

--- src/pages/DashboardSharedLayout.jsx
+++ src/pages/DashboardSharedLayout.jsx
@@ -1,11 +1,16 @@
 import { Outlet } from "react-router-dom";
-import { DashNavBar } from "../components/dashboard";
+import { DashNavBar, SidebarContainer } from "../components/dashboard";
+import { Footer } from "../components/common";
 
 const DashboardSharedLayout = () => {
   return (
     <>
       <DashNavBar />
-      <Outlet />
+      <div className='flex gap-12'>
+        <SidebarContainer />
+        <Outlet />
+      </div>
+      <Footer />
     </>
   );
 };
