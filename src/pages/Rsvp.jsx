e497f495dbc08d6176e08edde9532922857c4197
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-28 19:16:26 +0100

    rsvp page in progress

--- src/pages/Rsvp.jsx
+++ src/pages/Rsvp.jsx
@@ -1,12 +1,16 @@
 import { Card } from "../components/rsvp";
-
-// import { useLoaderData } from "react-router-dom";
+// import { useSearchParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 const Rsvp = () => {
-  // const receivedData = useLoaderData();
-  // const { data } = receivedData;
+  // const [searchParams] = useSearchParams();
+  // const data = Object.fromEntries([...searchParams]);
+  // console.log(data.guest);
+  const receivedData = useLoaderData();
+  const { data } = receivedData;
+  console.log("data from rsvp page", data);
 
   return (
-    <div className='bg-wybt-white px-5 flex justify-center'>
+    <div className='bg-wybt-white px-5 flex justify-center h-screen'>
       <Card />
     </div>
   );
