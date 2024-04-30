c0027ed245700f770c62181f226cc377654dddb7
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-30 07:58:22 +0100

    little fix

--- src/utils/formatDataTime.js
+++ src/utils/formatDataTime.js
@@ -1,8 +1,18 @@
 function formatDateTime(eventDate) {
-   const date = new Date(eventDate);
-   const months = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
+  const date = new Date(eventDate);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
   ];
 
   const month = months[date.getMonth()];
@@ -10,12 +20,12 @@ function formatDateTime(eventDate) {
   const year = date.getFullYear();
 
   let hours = date.getHours();
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const ampm = hours >= 12 ? 'PM' : 'AM';
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const ampm = hours >= 12 ? "PM" : "AM";
   hours = hours % 12;
   hours = hours ? hours : 12; // Handle the case when hours is 0 (12 AM)
 
-  return `${month} ${day}, ${year} ${hours}:${minutes}${ampm}`;
- }
+  return `${month} ${day}, ${year} ${hours} : ${minutes} ${ampm}`;
+}
 
- export default formatDateTime;
+export default formatDateTime;
