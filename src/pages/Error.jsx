848ca3cb7316d6e5b8deb391f14e658450bb1ac8
Akinniyi Ezekiel Wonderful <wonderfulakinniyi@gmail.com>
2024-04-30 12:55:27 +0100

    added error page and 401 error page as well

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,25 +1,21 @@
 import { Link } from "react-router-dom";
 
-const Error = () => {
+const Error = ({ error, title, text, path, pathText }) => {
   return (
     <div className='w-full bg-wybt-accent bg-opacity-15 z-10 p-5 md:p-20 min-h-screen flex flex-col lg:flex-row-reverse items-center justify-center  text-wybt-primary'>
       <div className='w-full lg:w-1/2 flex flex-col items-center justify-center lg:px-2 xl:px-0 text-center gap-4 md:gap-8'>
         <p className='text-7xl md:text-8xl lg:text-9xl font-bold tracking-wider '>
-          404
+          {error}
         </p>
         <p className='text-4xl md:text-5xl lg:text-6xl font-bold tracking-wider  mt-2'>
-          UH OH! You are lost.
+          {title}
         </p>
-        <p className='text-lg md:text-xl lg:text-2xl'>
-          The page you are looking for does not exist. How you got here is a
-          mystery. But you can click the button below to go back to the
-          homepage.
-        </p>
+        <p className='text-lg md:text-xl lg:text-2xl'>{text}</p>
         <Link
-          to='/'
+          to={path}
           className='flex items-center space-x-2 bg-wybt-primary text-wybt-white px-4 py-2 rounded transition duration-500'
         >
-          Return Home
+          {pathText}
         </Link>
       </div>
       <div className=' -z-10 w-full md:w-1/2 lg:h-full flex lg:items-end justify-center p-4'>
