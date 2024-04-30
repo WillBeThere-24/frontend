2acd66e9d266ceb560585c9c35ff65aefbbdd1a5
Sebastian Anioke <aniokechukwudi7@gmail.com>
2024-05-01 00:07:40 +0100

    ability to delete event

---
+++ src/utils/hooks/useDelete.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import axios from "axios";
+
+const useDelete = () => {
+  const [loading, setLoading] = useState(false);
+  const deleteData = async (url) => {
+    setLoading(true);
+    try {
+      const { data } = await axios.delete(url, {
+        withCredentials: true,
+      });
+      return data;
+    } catch (error) {
+      console.log(error.message)
+      throw new Error(error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { deleteData, loading };
+};
+
+export default useDelete;
